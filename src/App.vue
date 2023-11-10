9f60cd8f1e0a2b1e04cd4c4c1a4c6399f7e8ba27
umbertofaenza <umberto.faenza@gmail.com>
2023-11-10 17:50:59 +0100

    add Navbar and router-view to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
+import NavbarUi from './components/ui/NavbarUi.vue';
 
+export default {
+  components: {NavbarUi}
+}
 </script>
 
 <template>
-
+  <NavbarUi/>
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
