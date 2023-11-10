84a12a784819d352641c022f78269f8b52194d7d
umbertofaenza <umberto.faenza@gmail.com>
2023-11-10 17:51:21 +0100

    edit main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
-import { router } from "./router"
+import { createApp } from 'vue';
+import { router } from "./router";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
+import "./assets/scss/style.scss";
 
-import "./assets/scss/style.scss"
+import App from './App.vue';
 
-import App from './App.vue'
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
