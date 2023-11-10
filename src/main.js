7d54b16f447ad51062487ab64fb7b9db27b4f881
umbertofaenza <umberto.faenza@gmail.com>
2023-11-10 15:53:01 +0100

    setup

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import "./assets/scss/style.scss"
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
