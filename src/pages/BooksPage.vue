f38680a0a8df946daedc09cbdf71a22b805aa813
umbertofaenza <umberto.faenza@gmail.com>
2023-11-10 16:01:27 +0100

    add Home and Books pages

---
+++ src/pages/BooksPage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Books",
+    };
+  },
+};
+</script>
+
+<template>
+    <h1>{{title}}</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
